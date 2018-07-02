7424f21bbae74ec5732225813ad0510e78a91ccb
Playdata <Playdata@DESKTOP-3TARHPM>
2018-07-02 21:27:09 +0900

    yr hr

--- GitTest/src/test.java
+++ GitTest/src/test.java
@@ -1,13 +1,7 @@
 public class test {
 	public static void main(String[] args) {
 		System.out.println("각자 자신의 이름 입력 :");
-<<<<<<< HEAD
 		System.out.println("yr");
-=======
->>>>>>> refs/remotes/origin/dev
+		System.out.println("hr");
 	}
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> refs/remotes/origin/dev
