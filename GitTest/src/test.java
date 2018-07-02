30143232d3535be270146965dbe0c2f0b598da00
Seung Hwan <suhseunghwan2@gmail.com>
2018-07-02 20:29:26 +0900

    new one

--- GitTest/src/test.java
+++ GitTest/src/test.java
@@ -3,6 +3,7 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("각자 자신의 이름 입력 :");
 		System.out.println("서승환");
-		System.out.println("다음 사람 입력:");
+		System.out.println("다음 사람 입력해주세요");
+		
 	}
 }
