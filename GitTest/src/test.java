8a8bf6fedc26b85241c5b83d068c94ff58d31a64
Playdata <Playdata@DESKTOP-RD6FESM>
2018-07-02 21:30:31 +0900

    # WARNING: head commit changed in the meantime
    sd

--- GitTest/src/test.java
+++ GitTest/src/test.java
@@ -1,5 +1,7 @@
 public class test {
 	public static void main(String[] args) {
-		System.out.println("리셋된 메인");
+		System.out.println("媛곸옄 �옄�떊�쓽 �씠由� �엯�젰 :");
+		int a;
+		System.out.println("src");
 	}
 }
